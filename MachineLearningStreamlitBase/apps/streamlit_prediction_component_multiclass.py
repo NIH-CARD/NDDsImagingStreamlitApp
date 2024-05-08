6bf2c629f45b278867beb556e8400dec654c0219
b.pizarrogalleguillos <b.pizarrogalleguillos@gmail.com>
2024-05-07 22:07:27 -0400

    Explaining model parameters

--- MachineLearningStreamlitBase/apps/streamlit_prediction_component_multiclass.py
+++ MachineLearningStreamlitBase/apps/streamlit_prediction_component_multiclass.py
@@ -26,7 +26,7 @@ def app():
     st.experimental_set_query_params(
         show_map=True,
     )
-    st.markdown("""<style>.big-font {font-size:100px !important;}</style>""", unsafe_allow_html=True) 
+    st.markdown("""<style>.big-font {font-size:100px !important;}</style>""", unsafe_allow_html=True)
     st.markdown(
         """<style>
         .boxBorder {
@@ -36,9 +36,18 @@ def app():
             outline-offset: 5px;
             font-size:25px;
         }</style>
-        """, unsafe_allow_html=True) 
+        """, unsafe_allow_html=True)
     st.markdown('<div class="boxBorder"><font color="RED">Disclaimer: This predictive tool is only for research purposes</font></div>', unsafe_allow_html=True)
     st.write("## Model Perturbation Analysis")
+    st.write(
+        """
+        This predictive model utilizes parameters derived from T1w Brain MRI. 
+        The model parameters include measurments of area, thickness and volume of brain lobes, cortical and subcortical brain regions, basal forebrain, white matter, medial temporal lobe parcellation of hipocampal and extra-hippocampal regions, and cerebellum.
+        These regions are obtained from the CIT168 template (Subcortical brain nuclei), Desikan Killiany Tourville (DKT - Whole brain segmentation), among others atlas, and are implemented trough the ANTsPyMM library.
+
+         ANTsX neuroimaging-derived structural phenotypes of UK Biobank Nicholas J. Tustison et al. medRxiv 2023
+        """
+    )
 
     select_disease = st.selectbox("Select the disease", options=['ADRD', 'PD'])
     if select_disease == 'ADRD':
@@ -57,8 +66,8 @@ def app():
     numerical_columns = []
     X_new = X.fillna('Not available')
     for col in X_new.columns:
-            numerical_columns.append(col) 
-    
+            numerical_columns.append(col)
+
     uploaded_file = st.file_uploader("Upload nifti file")
     st.experimental_get_query_params()
     response = requests.get("https://antspyt1w-htco5r3hya-ue.a.run.app/justants")
@@ -75,13 +84,13 @@ def app():
         select_patient = list(X.index)[0]
         # select_patient = 904
 
-    select_patient_index = ids.index(select_patient) 
-    new_feature_input = defaultdict(list) 
+    select_patient_index = ids.index(select_patient)
+    new_feature_input = defaultdict(list)
 
     st.write('--'*10)
     st.write('##### Note: X denoted NA values')
     col1, col2, col3, col4 = st.columns(4)
-    
+
     for col in numerical_columns:
         X_new[col] = X_new[col].map(lambda x: float(x) if not x=='Not available' else np.nan)
 
@@ -89,7 +98,7 @@ def app():
         with col1:
             if (i+0) >= len(numerical_columns):
                 continue
-            c1 = numerical_columns[i+0] 
+            c1 = numerical_columns[i+0]
             idx = X_new.loc[select_patient, c1]
             # f1 = st.number_input("{}".format(feature_mapping[c1]), min_value=X_new[c1].min(),  max_value=X_new[c1].max(), value=idx)
             f1 = st.number_input("{}".format(feature_mapping[c1]), min_value=0.0,  max_value=1.0, value=idx)
@@ -98,33 +107,33 @@ def app():
         with col2:
             if (i+1) >= len(numerical_columns):
                 continue
-            c2 = numerical_columns[i+1] 
+            c2 = numerical_columns[i+1]
             idx = X_new.loc[select_patient, c2]
             f2 = st.number_input("{}".format(feature_mapping[c2]), min_value=0.0,  max_value=1.0, value=idx)
             new_feature_input[c2].append(f2)
         with col3:
             if (i+2) >= len(numerical_columns):
                 continue
-            c3 = numerical_columns[i+2] 
+            c3 = numerical_columns[i+2]
             idx = X_new.loc[select_patient, c3]
             f3 = st.number_input("{}".format(feature_mapping[c3]), min_value=0.0,  max_value=1.0, value=idx)
             new_feature_input[c3].append(f3)
         with col4:
             if (i+3) >= len(numerical_columns):
                 continue
-            c4 = numerical_columns[i+3] 
+            c4 = numerical_columns[i+3]
             idx = X_new.loc[select_patient, c4]
             f4 = st.number_input("{}".format(feature_mapping[c4]), min_value=0.0,  max_value=1.0, value=idx)
             new_feature_input[c4].append(f4)
-    
+
     st.write('--'*10)
     st.write("### Do you want to see the effect of changing a factor on this patient?")
     color_discrete_map = {}
     color_discrete_map_list = ["red", "green", "blue", "goldenred", "magenta", "yellow", "pink", "grey"]
     class_names = ['Control', select_disease]
     for e, classname in enumerate(class_names):
-        color_discrete_map[classname] = color_discrete_map_list[e] 
-    
+        color_discrete_map[classname] = color_discrete_map_list[e]
+
     show_whatif = st.checkbox("Enable what-if analysis")
     col01, col02 = st.columns(2)
     with col01:
@@ -227,7 +236,7 @@ def app():
             feature_print_what = ndfl.iloc[0].fillna('Not available')
             feature_print_what.index = feature_print_what.index.map(lambda x: feature_mapping[x])
             feature_print_what = feature_print_what.reset_index()
-            feature_print_what.columns = ["Feature Name", "Feature Value"] 
+            feature_print_what.columns = ["Feature Name", "Feature Value"]
             selected = []
             for i in range(len(feature_print_what)):
                 if feature_print.iloc[i]["Feature Value"] == feature_print_what.iloc[i]["Feature Value"]:
@@ -282,5 +291,3 @@ def app():
             # fig.savefig('/app/mar4_decisionplot.pdf', bbox_inches='tight')
             # fig.savefig('/app/mar4_decisionplot.eps', bbox_inches='tight')
             st.pyplot()
-
-
